db89e6bbb517bf48ac5a441fe948dabc56cc9b29
LDEV0422 <lsreg-pro@outlook.com>
2022-05-22 19:59:00 +0200

    Update + delete methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogComponent } from './components/dialog/dialog.component';
 import { ApiService } from './services/api.service';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -32,7 +32,12 @@ export class AppComponent implements OnInit {
   openDialog() {
     this.dialog.open(DialogComponent, {
       width: '30%'
-    });
+    }).afterClosed().subscribe(val => {
+      if (val === 'save') {
+        // callback method to get all products + new without having to refresh page after closing dialog
+        this.getAllProducts();
+      }
+    })
   }
 
   getAllProducts() {
@@ -45,6 +50,35 @@ export class AppComponent implements OnInit {
         },
         error: (err) => {
           alert("An error occurred while fetching the data")
+        }
+        // when complete
+        // complete:()=>{
+        // }
+      })
+  }
+
+  editProduct(row: any) {
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+      // pass the table row values as data to edit in the product form
+      data: row
+    }).afterClosed().subscribe(val => {
+      if (val === 'update') {
+        // callback method to get all products + update without having to refresh page after closing dialog
+        this.getAllProducts();
+      }
+    })
+  }
+
+  deleteProduct(id: number) {
+    this.apiService.deleteProduct(id)
+      .subscribe({
+        next:(response)=>{
+          alert("Product deleted successfully");
+          this.getAllProducts();
+        },
+        error:(err)=>{
+          alert("An error occurred while deleting the product")
         }
       })
   }
--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from 'src/app/services/api.service';
-import {MatDialogRef} from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-dialog',
@@ -12,41 +13,83 @@ export class DialogComponent implements OnInit {
 
   productConditionList = ["Brand New", "Second Hand", "Refurbished"];
   productForm !: FormGroup;
+  // to switch name of dialog button : edit or save
+  actionBtn: string = "Save";
 
-  constructor(private formBuilder : FormBuilder, private apiService : ApiService, private dialogRef : MatDialogRef<DialogComponent>) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiService: ApiService,
+    // to inject data in dialog window for edit method
+    @Inject(MAT_DIALOG_DATA) public editData: any,
+    private dialogRef: MatDialogRef<DialogComponent>
+  ) { }
 
   ngOnInit(): void {
-      // initialize form
+    // initialize form
     this.productForm = this.formBuilder.group({
-      name : ['', Validators.required],
-      category : ['', Validators.required],
-      date : ['', Validators.required],
-      condition : ['', Validators.required],
-      price : ['', Validators.required],
-      description : ['', Validators.required]
+      name: ['', Validators.required],
+      category: ['', Validators.required],
+      date: ['', Validators.required],
+      condition: ['', Validators.required],
+      price: ['', Validators.required],
+      description: ['', Validators.required]
     });
+
+    // pass product data in the form for edit method
+    if (this.editData) {
+      this.actionBtn = "Update";
+      this.productForm.controls['name'].setValue(this.editData.name);
+      this.productForm.controls['category'].setValue(this.editData.category);
+      this.productForm.controls['date'].setValue(this.editData.date);
+      this.productForm.controls['condition'].setValue(this.editData.condition);
+      this.productForm.controls['price'].setValue(this.editData.price);
+      this.productForm.controls['description'].setValue(this.editData.description);
+    }
   }
 
-  addProduct(){
-if (this.productForm.valid) {
-  this.apiService.postProduct(this.productForm.value)
-  .subscribe({
-    // if successful
-    next:(response)=>{
-      alert("Product added successfully");
-      this.productForm.reset();
-      this.dialogRef.close('save');
-    },
-    // if error
-    error:(err)=>{
-      alert("An error occurred while adding the product")
+  addProduct() {
+    // if the form is not on edit method, add new product
+    if (!this.editData) {
+      if (this.productForm.valid) {
+        this.apiService.postProduct(this.productForm.value)
+          .subscribe({
+            // if successful
+            next: (response) => {
+              alert("Product added successfully");
+              this.productForm.reset();
+              this.dialogRef.close('save');
+            },
+            // if error
+            error: (err) => {
+              alert("An error occurred while adding the product");
+            }
+            // when complete
+            // complete:()=>{ 
+            // }
+          })
+      }
+    } else {
+      this.updateProduct();
     }
-    // when complete
-    // complete:()=>{
+  }
 
-    // }
-  })
-}    
+  updateProduct(){
+    this.apiService.putProduct(this.productForm.value, this.editData.id)
+    .subscribe({
+      // if successful
+      next: (response) => {
+        alert("Product updated successfully");
+        this.productForm.reset();
+        this.dialogRef.close('update');
+      },
+      // if error
+      error: (err) => {
+        alert("An error occurred while updating the product");
+      }
+      // when complete
+      // complete:()=>{
+      // }
+    })
   }
 
 }
